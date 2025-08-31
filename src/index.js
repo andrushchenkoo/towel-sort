fed3556e67229b13649d5c53dc3d6341f2785208
Oleksandr Andrushchenko <aandrushchenko1402@gmail.com>
2025-08-31 17:17:50 +0300

    feat: task-solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const result = [];
+
+  if (!matrix) return [];
+
+  const copyMatrix = [...matrix];
+
+  copyMatrix.forEach((row, index) => {
+    if (index % 2 !== 0) {
+      row.reverse();
+    }
+    row.forEach((element) => {
+      result.push(element);
+    });
+  });
+
+  return result;
 };
